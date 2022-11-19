6662ef02271cb569ced57d80c8c64ef970dda6fe
tim silber <tim.silber@me.com>
2022-11-19 15:32:12 -0500

    Start 11ty templating

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig){
+
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('/src/assets')
+
     return {
         dir: {
             input: "src",
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 11ty demo
 
 How quickly can we spin up a markdown SSG with a headless CMS via JAMStack?
+
+install nunjucks extension for vscode
+select nunjucks from bottom toolbar, set language to html
